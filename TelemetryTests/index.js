6056e7cdbc26458833459ef40b7bf5595ced622a
zainulmustafa <zain2984@gmail.com>
2022-08-26 12:30:13 +0500

    argument based control for docker

--- TelemetryTests/index.js
+++ TelemetryTests/index.js
@@ -1,14 +1,24 @@
 const gpmfExtract = require('gpmf-extract');
 const goproTelemetry = require(`gopro-telemetry`);
 const fs = require('fs');
+const path = require('path');
 
-const file = fs.readFileSync('./samples/sample.mp4');
+const args = process.argv.slice(2);
+const filePath = args[0];
+const fileName = path.basename(filePath)
+console.log(`Processing ${fileName}`)
+if (fs.existsSync(filePath)) {
+    // './samples/GX010115.MP4'
+    const file = fs.readFileSync(filePath);
 
-gpmfExtract(file)
-    .then(extracted => {
-        goproTelemetry(extracted, {}, telemetry => {
-            fs.writeFileSync('output_path.json', JSON.stringify(telemetry));
-            console.log('Telemetry saved as JSON');
-        });
-    })
-    .catch(error => console.error(error));
+    gpmfExtract(file)
+        .then(extracted => {
+            goproTelemetry(extracted, {}, telemetry => {
+                fs.writeFileSync(`${fileName}.json`, JSON.stringify(telemetry));
+                console.log('Telemetry saved as JSON');
+            });
+        })
+        .catch(error => console.error(error));
+} else {
+    console.log("File not found!")
+}
